ea445952a5887c667c72390c0856858d5b894ea5
GokhanCingoz <gokhancingoz1@gmail.com>
2023-03-19 15:05:33 +0300

    Like Button added. Model View Controller added. Like quantity added

--- Bloggie.Web/Controllers/BlogsController.cs
+++ Bloggie.Web/Controllers/BlogsController.cs
@@ -1,22 +1,51 @@
-﻿using Bloggie.Web.Repositories;
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class BlogsController : Controller
-    {   
+    {
         private readonly IBlogPostRepository blogPostRepository;
-        public BlogsController(IBlogPostRepository blogPostRepository)
+        private readonly IBlogPostLikeRepository blogPostLikeRepository;
+
+        public BlogsController(IBlogPostRepository blogPostRepository, IBlogPostLikeRepository blogPostLikeRepository)
         {
-           this.blogPostRepository = blogPostRepository;
+            this.blogPostRepository = blogPostRepository;
+            this.blogPostLikeRepository = blogPostLikeRepository;
         }
-       
 
         [HttpGet]
         public async Task<IActionResult> Index(string urlHandle)
-        {   
-            var blogpost = await blogPostRepository.GetByIdUrlHandleAsync(urlHandle);
-            return View(blogpost);
+        {
+            var blogPost = await blogPostRepository.GetByIdUrlHandleAsync(urlHandle);
+            var blogDetailsViewModel = new BlogDetailsViewModel();
+
+            if (blogPost != null)
+            {
+                var totalLikes = await blogPostLikeRepository.GetTotalLikes(blogPost.Id);
+
+                blogDetailsViewModel = new BlogDetailsViewModel
+                {
+                    Id = blogPost.Id,
+                    Content = blogPost.Content,
+                    PageTitle = blogPost.PageTitle,
+                    Author = blogPost.Author,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    Heading = blogPost.Heading,
+                    PublishedDate = blogPost.PublishedDate,
+                    ShortDescription = blogPost.ShortDescription,
+                    UrlHandle = blogPost.UrlHandle,
+                    Visible = blogPost.Visible,
+                    Tags = blogPost.Tags,
+                    TotalLikes = totalLikes
+                };
+            }
+
+            return View(blogDetailsViewModel);
+
         }
+
+
     }
 }
---
+++ Bloggie.Web/Repositories/BlogPostLikeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Bloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Repositories
+{
+    public class BlogPostLikeRepository : IBlogPostLikeRepository
+    {
+        private readonly BloggieDbContext bloggieDbContex;
+
+       public BlogPostLikeRepository(BloggieDbContext bloggieDbContext)
+        {
+           this.bloggieDbContex = bloggieDbContext;
+        }
+
+    
+
+        public async Task<int> GetTotalLikes(Guid blogPostId)
+        {
+            return await bloggieDbContex.BlogPostLikes.CountAsync(x=>x.BlogPostId==blogPostId);
+        }
+    }
+}
